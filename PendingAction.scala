cf8a82ba1c871a6f3c5df80a874d0384b29ffc1c
Grumpy Old Man63 <ian.j63@bigpond.com>
2024-11-09 09:31:47 +1100

    airline-web/app/models

---
+++ PendingAction.scala
@@ -0,0 +1,15 @@
+package models
+
+import com.patson.model.Airline
+
+case class PendingAction(airline : Airline, category : PendingActionCategory.Value)
+
+object PendingActionCategory extends Enumeration {
+  type AlertCategory = Value
+  val OLYMPICS_VOTE = Value
+  val ALLIANCE_PENDING_APPLICATION = Value
+}
+
+
+
+
